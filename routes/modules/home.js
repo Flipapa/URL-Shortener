a95ac3bb65ca22c59cb1179c60e4745bdf302164
Flipapa <roamer2010x@gmail.com>
2021-07-28 03:15:01 +0800

    feat: add avoid-repeat-shortenedURL function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -10,19 +10,28 @@ router.get('/', (req, res) => {
 router.post('/', async (req, res) => {
   const { inputURL } = req.body
   const record = await ShortedURL.findOne({ targetURL: inputURL }).lean()
+
   let randomCode
   let shortedURL
+
   if (record) {
     randomCode = record.randomCode
   } else {
     randomCode = generateRandomCode()
+    let codeCheck = await ShortedURL.findOne({ randomCode }).lean()
+    while (codeCheck) {
+      randomCode = generateRandomCode()
+      codeCheck = await ShortedURL.findOne({ randomCode })
+    }
     await ShortedURL.create({ randomCode, targetURL: inputURL })
   }
+
   if (req.headers.host === 'localhost:3000') {
     shortedURL = `http://${req.headers.host}/${randomCode}`
   } else {
     shortedURL = `https://${req.headers.host}/${randomCode}`
   }
+  
   res.render('index', { shortedURL })
 })
 
